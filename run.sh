0bd2d4bdee1da5059e33a87d978c1c4b6019a735
Gudsfile <gudsfile@protonmail.com>
2020-12-13 17:02:03 +0100

    🔥 remove nlp requirements

--- run.sh
+++ run.sh
@@ -12,14 +12,6 @@ pip install --upgrade pip
 echo "Installation des bibliothèques python"
 pip install -r requirements.txt
 
-echo "Installation de la librairie fr pour spacy"
-spacy info fr_core_news_sm
-if [ $? != 0 ]; then
-  python -m spacy download fr_core_news_sm
-else
-  echo "Librairie déjà installée"
-fi
-
 echo "Mise à jour des varibles d'environnements"
 export DISCORD_TOKEN=$bot_token
 
